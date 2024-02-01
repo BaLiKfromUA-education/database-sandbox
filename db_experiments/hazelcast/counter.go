a1a8fa4f592e4649d092d397321be48584f3e696
Valentyn Yukhymenko <valentin.yukhymenko@gmail.com>
2024-02-01 23:18:48 +0000

    refactoring and logs for slow case

--- db_experiments/hazelcast/counter.go
+++ db_experiments/hazelcast/counter.go
@@ -94,6 +94,37 @@ func (dao *CounterDao) counterWithoutBlockingImpl(ctx context.Context, key strin
 	}
 }
 
+func lockAndIncrement(ctx context.Context, distMap *hazelcast.Map, key string) {
+	intValue := int64(0)
+	// Create a new unique lock context.
+	// https://pkg.go.dev/github.com/hazelcast/hazelcast-go-client#hdr-Using_Locks
+	lockCtx := distMap.NewLockContext(ctx)
+	// Lock the key.
+	// The key cannot be unlocked without the same lock context.
+	if err := distMap.Lock(lockCtx, key); err != nil {
+		log.Fatalf("Error on Lock: %v", err)
+	}
+	// Remember to unlock the key, otherwise it won't be accessible elsewhere.
+	defer distMap.Unlock(lockCtx, key)
+	// The same lock context, or a derived one from that lock context must be used,
+	// otherwise the Get operation below will block.
+	v, err := distMap.Get(lockCtx, key)
+	if err != nil {
+		log.Fatalf("Error on Get: %v", err)
+	}
+	// If v is not nil, then there's already a value for the key.
+	if v != nil {
+		intValue = v.(int64)
+	}
+	// Increment and set the value back.
+	intValue++
+	// The same lock context, or a derived one from that lock context must be used,
+	// otherwise the Set operation below will block.
+	if err = distMap.Set(lockCtx, key, intValue); err != nil {
+		log.Fatalf("Error on Set: %v", err)
+	}
+}
+
 func (dao *CounterDao) counterWithPessimisticBlockingImpl(ctx context.Context, key string, distMap *hazelcast.Map, wg *sync.WaitGroup) {
 	defer wg.Done()
 
@@ -101,34 +132,8 @@ func (dao *CounterDao) counterWithPessimisticBlockingImpl(ctx context.Context, k
 		if i%100 == 0 {
 			log.Printf("[Pessimistic] At %d\n", i)
 		}
-
-		// https://pkg.go.dev/github.com/hazelcast/hazelcast-go-client#hdr-Using_Locks
-		lockCtx := distMap.NewLockContext(ctx)
-
-		err := distMap.Lock(lockCtx, key)
-		if err != nil {
-			log.Fatalf("Error on Lock: %v", err)
-		}
-
-		counter, err := distMap.Get(lockCtx, key)
-		if err != nil {
-			_ = distMap.Unlock(lockCtx, key)
-			log.Fatalf("Error on Get: %v", err)
-		}
-
-		cnt := counter.(int64)
-		cnt += 1
-
-		err = distMap.Set(lockCtx, key, cnt)
-		if err != nil {
-			_ = distMap.Unlock(lockCtx, key)
-			log.Fatalf("Error on Set: %v", err)
-		}
-
-		err = distMap.Unlock(lockCtx, key)
-		if err != nil {
-			log.Fatalf("Error of Unlock: %v", err)
-		}
+		// taken from https://github.com/hazelcast/hazelcast-go-client/blob/e7a962174982d98a3e3840ab3bec917bf67596a0/examples/map/lock/main.go
+		lockAndIncrement(ctx, distMap, key)
 	}
 }
 
